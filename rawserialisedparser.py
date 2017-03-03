14ed5dc788e88fe8065350ac90b9a90506514f5d
yda288 <yda288@users.noreply.github.com>
2017-03-03 15:30:53 +0000

    Refactor Parser into class.

--- rawserialisedparser.py
+++ rawserialisedparser.py
@@ -1,58 +1,57 @@
 
 from rawserialised import *
 from boostmappings import mappings
-# def walk_args(func):
-#     #filter(lambda f: isinstance(f, FuncDef), funcs)
-#     for arg in func.arguments:
-#         if arg.variable:
-#
-#         func.arguments[1].variable.type.type.fullname()
-#         func.arguments[0].variable.type.args[0].items[0].type.fullname()
-#
-#         arg.variable
 
-# if 'args' in arg:
-#     chain = mapped(arg['type_ref']) + str([chain_nested(a) for a in arg['args']])
-#     nested_args.append(chain)
-# else:
-#     chain = chain_nested(arg)
-#     nested_args.append(chain)
+class Parser:
+    def __init__(self, funcs):
+        self.funcs = funcs
+        self.parsed = []
 
-def parse(serialised):
-    nested_args = []
-    nested_ret = []
+    def parse(self):
+        for func in self.funcs:
+            self.parsed.append(Parser._parse(func))
 
-    ##parse arg types
-    try:
-        for arg in serialised['type']['arg_types']:
-            chain = chain_nested(arg)
-            nested_args.append(chain)
-    except:
-        nested_args.append('None')
+    ##static methods below as they do not require instance state
+    @staticmethod
+    def _parse(serialised):
+        nested_args = []
+        nested_ret = []
 
-    ##parse return types
-    try:
-        nested_ret.append(chain_nested(serialised['type']['ret_type']))
-    except:
-        nested_ret.append('None')
-    return (nested_ret, nested_args)
+        ##parse arg types
+        try:
+            for arg in serialised['type']['arg_types']:
+                chain = Parser._chain_nested(arg)
+                nested_args.append(chain)
+        except:
+            nested_args.append('None')
 
-def chain_nested(arg):
-    try:
-        if 'args' in arg:
-            return '%s%s' % (mapped(arg['type_ref']),[chain_nested(a) for a in arg['args']])
-        elif 'items' in arg:
-            return '%s%s' % (mapped(arg['fallback']['type_ref']), [chain_nested(i) for i in arg['items']])
-        elif 'item' in arg:
-            return chain_nested(arg['item'])
-        else:
-            return mapped(arg['type_ref'])
-    except:
-        return 'None'
+        ##parse return types
+        try:
+            nested_ret.append(Parser._chain_nested(serialised['type']['ret_type']))
+        except:
+            nested_ret.append('None')
+        return (nested_ret, nested_args)
 
-def mapped(builtin):
-    return mappings.get(builtin, builtin)
+    @staticmethod
+    def _chain_nested(arg):
+        try:
+            if 'args' in arg:
+                return '%s%s' % (Parser._mapped(arg['type_ref']),[Parser._chain_nested(a) for a in arg['args']])
+            elif 'items' in arg:
+                return '%s%s' % (Parser._mapped(arg['fallback']['type_ref']), [Parser._chain_nested(i) for i in arg['items']])
+            elif 'item' in arg:
+                return Parser._chain_nested(arg['item'])
+            else:
+                return Parser._mapped(arg['type_ref'])
+        except:
+            return 'None'
+
+    @staticmethod
+    def _mapped(builtin):
+        return mappings.get(builtin, builtin)
+
 
 if __name__ == '__main__':
-    p = parse(inproduct)
-    print(p)
+    parser = Parser([inproduct])
+    parser.parse()
+    print(parser.parsed)
